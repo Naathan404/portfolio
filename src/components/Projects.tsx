664f8e990bff01d5b3b6cf3c7d274126c7b0e309
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-06-08 22:32:33 +0700

    update tile trip project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,8 +11,8 @@ const unityProjects = [
     tech: ['C#', 'Custom Editor Tool', 'JSON', 'ScriptableObjects', 'DOTween'],
     github: 'https://github.com/Naathan404/TileTripPuzzle',
     demo: 'https://naathan404.itch.io/tile-trip-puzzle',
-    video: 'https://youtube.com/shorts/Esmn-DsElBw',
-    images: ['/images/tile-trip-1.png','/images/tile-trip-2.png','/images/tile-trip-3.png', '/images/tile-trip-4.png', '/images/tile-trip-5.png'],
+    video: 'https://youtube.com/shorts/f0luBTxtHh0?feature=share',
+    images: ['/images/tile-trip-3.png', '/images/tile-trip-0.png', '/images/tile-trip-2.png','/images/tile-trip-4.png', '/images/tile-trip-5.png', '/images/tile-trip-6.png'],
     role: '\n🛠️ Built a custom Unity Editor Tool for visual level layout design with JSON-based save/load.\n🧩 Implemented a backward-generation algorithm guaranteeing at least one valid winning path per level.\n✨ Enhanced game feel using DOTween animations and Unity Particle System for match feedback.'
   },
   {
